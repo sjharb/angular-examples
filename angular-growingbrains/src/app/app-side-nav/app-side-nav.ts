e8cfd8484cc6c6e0d8fca88c23c30ce31aa1863a
sjharb <zzmharbison@gmail.com>
2025-07-15 16:10:32 -0500

    Added dynamic response div element creation.

--- angular-growingbrains/src/app/app-side-nav/app-side-nav.ts
+++ angular-growingbrains/src/app/app-side-nav/app-side-nav.ts
@@ -4,8 +4,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { JsonRequest } from '../json-request/json-request';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectionList } from '@angular/material/list';
 import { MatListOption } from '@angular/material/list';
 import { MatOption } from '@angular/material/autocomplete';
@@ -14,13 +14,14 @@ import { MatSelect } from '@angular/material/select';
 import { ThemeController } from "../theme-controller/theme-controller";
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '../theme-service/theme-service';
+import { MatIconModule } from '@angular/material/icon';
 //import { MatListItem } from "../../../node_modules/@angular/material/list/index";
 //import { llmModels } from '../json-request-service/json-request-service';
 
 
 @Component({
   selector: 'app-side-nav',
-  imports: [CommonModule, MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule, JsonRequest, MatInputModule, MatFormFieldModule, MatSelect, MatOption, ThemeController],
+  imports: [MatIconModule, CommonModule, MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule, JsonRequest, MatInputModule, MatFormFieldModule, MatSelect, MatOption, ThemeController],
   templateUrl: './app-side-nav.html',
   styleUrl: './app-side-nav.css'
 })
@@ -32,12 +33,13 @@ export class AppSideNav {
   events: string[] = [];
   opened: boolean = true;
   public llmServerAddressValue: string = '';
-  
+
   isNavButtonDisabled: boolean = false
-  sideNavOptionsButtonText: string = 'Options';
+  sideNavOptionsButtonText: string = 'Close Options';
   mainOptionsButtonText: string = 'Options';
+  mainOptionsButtonIconName: string = 'more';
   //currentTheme: any;
-  
+
   themeService = inject(ThemeService);
 
   // constructor() {
@@ -47,12 +49,14 @@ export class AppSideNav {
   // }
 
   sideNavOpened() {
-    this.mainOptionsButtonText = ''
+    //this.mainOptionsButtonText = ''
+    this.mainOptionsButtonIconName = '';
     this.isNavButtonDisabled = true;
   }
 
   sideNavClosed() {
-    this.mainOptionsButtonText = '<< Options'
+    //this.mainOptionsButtonText = '<<'
+    this.mainOptionsButtonIconName = 'more';
     this.isNavButtonDisabled = false;
   }
 
--- angular-growingbrains/src/app/json-request/json-request.ts
+++ angular-growingbrains/src/app/json-request/json-request.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentRef, createComponent, effect, inject, Input, input, signal, SimpleChanges } from '@angular/core';
+import { Component, ComponentRef, createComponent, effect, inject, Input, input, signal, SimpleChanges, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { JsonRequestService, RequestData, ResponseData, MessageData, LlmModels } from '../json-request-service/json-request-service';
@@ -30,6 +30,15 @@ import { ThemeService } from '../theme-service/theme-service';
   styleUrl: './json-request.css'
 })
 export class JsonRequest {
+  // public renderer: Renderer2;
+  // private el!: ElementRef;
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  //@ViewChild('responseContainer')
+  //@ViewChild('myElement', { static: false }) myElementRef: ElementRef<HTMLDivElement>;
+
+  @ViewChild('responseContainer', { static: false }) responseDiv!: ElementRef<HTMLDivElement>;
+
   modelValue: string = "";
 
   private jsonRequestService = inject(JsonRequestService);
@@ -52,7 +61,7 @@ export class JsonRequest {
     } else {
       this.modelValue = this.selectedLlmModel;
     }
-    
+
     console.log("ngOnChanges: this.modelValue = " + this.modelValue);
   }
 
@@ -151,6 +160,7 @@ export class JsonRequest {
     //this.promptResponse.update(currentPrompts => [...currentPrompts, "Prompt: '" + promptValue.prompt + "'\n"]);
     //this.promptResponse.set("Prompt:\n'" + promptValue.prompt + "'\n");
     this.promptResponse.update(currentPrompts => currentPrompts + "Prompt:\n'" + requestData.prompt + "'\n");
+    this.addRequestDiv(requestData);
     this.jsonRequestService.sendData(requestData).subscribe({
       next: (responseData: ResponseData) => {
         this.chatMesages.push(responseData.message);
@@ -159,9 +169,11 @@ export class JsonRequest {
         //this.promptResponse.set("Response:\n'" + responseData.response + "'\n");
         this.promptResponse.update(currentPrompts => currentPrompts + "\n'" + responseData.message.content + "'\n");
 
-        console.log(responseData);
-        console.log(responseData.response);
+        console.log("responseData = " + responseData);
+        console.log("responseData.response = " + responseData.response);
         this.statusMessage = 'Waiting for another prompt...';
+
+        this.addResponseDiv(responseData);
       },
       error: (error) => {
         console.error('Error creating item:', error);
@@ -171,23 +183,78 @@ export class JsonRequest {
   }
 
   clearResponseText() {
+    // Old response div clear. TODO: Remove me.
     //this.promptResponse.set([]);
     this.promptResponse.set('');
+
+    //For the new response div.
+    this.removeAllDivChildren(this.responseDiv);
   }
 
-  // async addComponent() {
-  //   class RootComponent {}
-  //   const applicationRef = await bootstrapApplication(RootComponent);
-  //   const hostElement = document.getElementById('dynamicComonent');
-  //   const environmentInjector = applicationRef.injector;
-  //   const componentRef = createComponent(JsonRequest, {hostElement, environmentInjector});
-  //   applicationRef.attachView(componentRef.hostView);
-  //   componentRef.changeDetectorRef.detectChanges();
-  // }
+  removeAllDivChildren(elementRef: ElementRef) {
+    if (elementRef) {
+      const children = Array.from(elementRef.nativeElement.children);
+      children.forEach(child => {
+        this.renderer.removeChild(elementRef.nativeElement, child);
+      });
+    }
+  }
 
-  // let myObservable = this.jsonRequestService.sendData(JsonRequest.requestData);
-  // promptResponse = myObservable[response];
-  //}
+  addRequestDiv(requestData: RequestData) {
+    // const div = this.renderer.createElement('div');
+    // const p = this.renderer.createElement('p');
+    // this.renderer.appendChild(div, p);
+    // const text = this.renderer.createText("Prompt:\n'" + requestData.prompt + "'");
+    // this.renderer.appendChild(p, text);
+    const div = this.renderer.createElement('div', 'request-div-body');
+    const bLabel = this.renderer.createElement('b');
+    this.renderer.appendChild(div, bLabel);
+    const labelText = this.renderer.createText("Prompt:");
+    this.renderer.appendChild(bLabel, labelText);
+    const pContent = this.renderer.createElement('p');
+    this.renderer.appendChild(div, pContent);
+    const contentText = this.renderer.createText("'" + requestData.prompt + "'");
+    this.renderer.appendChild(pContent, contentText);
+    this.renderer.appendChild(this.responseDiv.nativeElement, div);
+  }
+
+  addResponseDiv(responseData: ResponseData) {
+    let thinkMessageSection: string = responseData.message.content.split('</think>')[0];
+    let responseMessageSection: string = responseData.message.content.split('</think>')[1];
+    thinkMessageSection = thinkMessageSection.split("<think>")[1];
+    const div = this.renderer.createElement('div');
+    const bLabel = this.renderer.createElement('b');
+    this.renderer.appendChild(div, bLabel);
+    const labelText = this.renderer.createText("Response:");
+    this.renderer.appendChild(bLabel, labelText);
+    //const pThinkContent = this.renderer.createElement('p');
+    //this.renderer.appendChild(div, pThinkContent);
+    const iThinkContent = this.renderer.createElement('i');
+    //this.renderer.appendChild(pThinkContent, iThinkContent);
+    this.renderer.appendChild(div, iThinkContent);
+    const thinkText = this.renderer.createText(thinkMessageSection);
+    this.renderer.appendChild(iThinkContent, thinkText);
+    const pResponseContent = this.renderer.createElement('p');
+    this.renderer.appendChild(div, pResponseContent);
+    const responseText = this.renderer.createText(responseMessageSection);
+    this.renderer.appendChild(pResponseContent, responseText);
+    console.log("thinkMessageSection = " + thinkMessageSection);
+    console.log("responseMessageSection = " + responseMessageSection);
+    // console.log("this.el = " + this.el);
+    // console.log("this.el.nativeElement = " + this.el.nativeElement);
+    // console.log("this.el.nativeElement.querySelector('#responseContainer') = " + this.el.nativeElement.querySelector('#responseContainer'));
+    // console.log("div = " + div);
+    // console.log("this.responseDiv = " + this.responseDiv);
+    // console.log("this.renderer.appendChild = " + this.renderer.appendChild);
+    //@ViewChild('responseContainer') myDiv: ElementRef;
+    //@ViewChild('myElement', { static: false }) myElementRef: ElementRef<HTMLDivElement>;
+    //@ViewChild('responseContainer');
+    //this.renderer.appendChild(this.el.nativeElement.querySelector('#responseContainer'), div);
+    //this.renderer.appendChild(this.responseDiv.nativeElement, div);
+    this.renderer.appendChild(this.responseDiv.nativeElement, div);
+    //this.renderer.appendChild(div, div);
+    //console.log("addResponseDiv");
+  }
 }
 
 
